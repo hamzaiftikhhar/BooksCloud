4522593ed0d249d5dcff24d6b1d3b11c81d96f66
hamzaiftikhhar <alihamxa366@gmail.com>
2026-05-12 14:14:18 +0500

    written test cases

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,19 +2,31 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   it { should belong_to(:author) }
 
-  it "is valid with a title" do
-    book = Book.new(title: "Atomic Habits")
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:isbn) }
+  it { should validate_presence_of(:description) }
+
+  it "is valid with valid attributes" do
+    author = Author.create!(first_name: "J.K", last_name: "Rowling")
+
+    book = Book.new(
+      title: "Atomic Habits",
+      author_id: author.id,
+      isbn: "9783161484100",
+      description: "Good book",
+      publication_date: Date.current,
+      total_copy_count: 10,
+      available_copy_count: 10,
+      genre: 3
+    )
 
     expect(book).to be_valid
   end
+
 
   it "is invalid without a title" do
     book = Book.new(title: nil)
 
     expect(book).not_to be_valid
-  end
-
-  RSpec.describe Book, type: :model do
-  it { should belong_to(:author) }
   end
 end
